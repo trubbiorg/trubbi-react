4f087c2bef20b4a6a6711099ec60cbe809783d9e
asfelizzola <asfelizzola@gmail.com>
2022-05-29 02:25:30 -0300

    loginAdmin route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/login.jsx';
 import Home from './pages/home.jsx';
 import Opinions from './pages/opinions.jsx'
+import Providers from './pages/providers.jsx';
+import LoginAdmin from './pages/loginadmin.jsx';
 import './App.css';
 
 const App = () => {
@@ -23,6 +25,7 @@ const App = () => {
         {(loggedIn) ? <Route path="/" element={<Home loggedIn={loggedIn} onLoggedOut={logOut} />} /> : <Route path="/" element={<Login onLoggedIn={logIn} />} />}
         <Route path="/opinions" element={<Opinions loggedIn={loggedIn} onLoggedOut={logOut}/>}/>
         <Route path='/providers' element={<Providers loggedIn={loggedIn} onLoggedOut={logOut}/>}/>
+        <Route path='/loginadmin' element={<LoginAdmin loggedIn={loggedIn} onLoggedOut={logOut}/>}/>
       </Routes>
     </BrowserRouter>
   );
