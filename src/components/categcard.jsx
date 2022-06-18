6a46c11ddeb464cdb8fb4a3b1722776649e48073
sabisabi00 <julysabininaba@gmail.com>
2022-06-18 14:39:40 -0300

    eliminar categorias

--- src/components/categcard.jsx
+++ src/components/categcard.jsx
@@ -5,8 +5,26 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import genericDataService from '../helpers/genericDataService';
+import { useNavigate } from 'react-router-dom';
+
+const categoriesDataService = new genericDataService("/categories");
 
 export default function CategCard(props){
+
+    const navigate = useNavigate();
+
+
+      const destroy = () => {
+        console.log("Llegó")
+        categoriesDataService.destroy(props.category.id).then(
+          response => response
+        ).catch(
+          response=>console.log(response.data)
+        )
+        navigate("/categories")
+      }
+
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardContent>
@@ -14,7 +32,8 @@ export default function CategCard(props){
         {props.category.name}
         </Typography>
       <CardActions>
-        <Button size="small" sx={{ marginLeft: 30 }} style={{ color: 'red' }}>Eliminar</Button>
+        <Button size="small" sx={{ marginLeft: 30 }} style={{ color: 'red' }} 
+        onClick={destroy} >Eliminar</Button>
       </CardActions>
       </CardContent>
     </Card>
